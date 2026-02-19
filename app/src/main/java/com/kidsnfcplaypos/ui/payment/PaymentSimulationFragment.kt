5870a33c358be3974c8d672da60ae05ee7ced191
sergitorralba <sergi.torralba@gmail.com>
2026-02-19 23:13:23 +0100

    fix: Add missing activityViewModels import in PaymentSimulationFragment

--- app/src/main/java/com/kidsnfcplaypos/ui/payment/PaymentSimulationFragment.kt
+++ app/src/main/java/com/kidsnfcplaypos/ui/payment/PaymentSimulationFragment.kt
@@ -17,6 +17,7 @@ import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
